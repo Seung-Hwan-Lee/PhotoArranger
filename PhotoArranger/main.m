2e5e68400f1916a10c65fd5ce3cc2142ec812f3b
Seung-Hwan Lee <shwan.lee@linecorp.com>
2016-12-30 21:25:31 +0900

    Initial Commit

---
+++ PhotoArranger/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhotoArranger
+//
+//  Created by Seung-Hwan Lee on 2016. 12. 30..
+//  Copyright © 2016년 Seung-Hwan Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
